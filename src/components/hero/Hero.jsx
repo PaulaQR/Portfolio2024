dd71cb90fdced14f9097cf80674e14bd47b623c4
María Paula Quintero Rodriguez <79113076+PaulaQR@users.noreply.github.com>
2023-10-31 12:05:20 -0500

    adding animation to the hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,19 +1,70 @@
-import "./Hero.scss"
+import "./Hero.scss";
+import	{ motion } from "framer-motion";
+
+const textVariants = {
+	initial:{
+		x: -500,
+		opacity: 0,
+		},
+		animate:{
+			x:0,
+			opacity:1,
+			transition:{
+				duration:1,
+				staggerChildren: 0.1,
+		 },
+	},
+	scrollButton:{
+		opacity:0,
+		y: 10,
+		transition:{
+			duration:2,
+			repeat:Infinity
+		}
+	},
+};
+
+const sliderVariants = {
+	initial:{
+		x: 0,
+		},
+		animate:{
+			x:"-220%",
+			transition:{
+				repeat: Infinity,
+				repeatType: "mirror",
+				duration: 20,
+		 },
+	},
+};
 
 const Hero = () => {
 		return (
 				<div className="hero">
 					<div	className="wrapper">
-					<div className="textContainer">
-						<h2>PAULA QUINTERO</h2>
-						<h1>Web designer</h1>
-						<div className="buttons">
-							<button>See the latest Works</button>
-							<button>Contact Me</button>
-							</div>
-						</div>
-						<img src="/scroll.png" alt="scroll" />
+					<motion.div
+					 className="textContainer"
+						 variants={textVariants}
+							initial="initial"
+							animate="animate"
+							>
+						<motion.h2 variants={textVariants}>PAULA QUINTERO</motion.h2>
+						<motion.h1 variants={textVariants}>Web designer</motion.h1>
+						<motion.div variants={textVariants} className="buttons">
+							<motion.button variants={textVariants}>See the latest Works</motion.button>
+							<motion.button variants={textVariants}>Contact Me</motion.button>
+							</motion.div>
+						<motion.img variants={textVariants} animate="scrollButton" src="/scroll.png" alt="scroll" />
+						</motion.div>
 					</div>
+					<motion.div
+					 className="slidingTextContainer"
+						 variants={sliderVariants}
+							initial="initial"
+							animate="animate"
+							>
+						Creator of digital pieces and web content
+					</motion.div>
 					<div className="imageContainer">
 						<img	src="/hero.png" alt="Imagen Home" />
 					</div>
