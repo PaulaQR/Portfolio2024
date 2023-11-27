2d5addae02358517050a45abc0411afead2b63f7
María Paula Quintero Rodriguez <79113076+PaulaQR@users.noreply.github.com>
2023-11-27 08:02:45 -0500

    Add component Portfolio

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,75 +1,75 @@
 import "./Hero.scss";
-import	{ motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 const textVariants = {
-	initial:{
+	initial: {
 		x: -500,
 		opacity: 0,
-		},
-		animate:{
-			x:0,
-			opacity:1,
-			transition:{
-				duration:1,
-				staggerChildren: 0.1,
-		 },
 	},
-	scrollButton:{
-		opacity:0,
+	animate: {
+		x: 0,
+		opacity: 1,
+		transition: {
+			duration: 1,
+			staggerChildren: 0.1,
+		},
+	},
+	scrollButton: {
+		opacity: 0,
 		y: 10,
-		transition:{
-			duration:2,
-			repeat:Infinity
+		transition: {
+			duration: 2,
+			repeat: Infinity
 		}
 	},
 };
 
 const sliderVariants = {
-	initial:{
+	initial: {
 		x: 0,
+	},
+	animate: {
+		x: "-220%",
+		transition: {
+			repeat: Infinity,
+			repeatType: "mirror",
+			duration: 20,
 		},
-		animate:{
-			x:"-220%",
-			transition:{
-				repeat: Infinity,
-				repeatType: "mirror",
-				duration: 20,
-		 },
 	},
 };
 
 const Hero = () => {
-		return (
-				<div className="hero">
-					<div	className="wrapper">
-					<motion.div
-					 className="textContainer"
-						 variants={textVariants}
-							initial="initial"
-							animate="animate"
-							>
-						<motion.h2 variants={textVariants}>PAULA QUINTERO</motion.h2>
-						<motion.h1 variants={textVariants}>Web designer</motion.h1>
-						<motion.div variants={textVariants} className="buttons">
-							<motion.button variants={textVariants}>See the latest Works</motion.button>
-							<motion.button variants={textVariants}>Contact Me</motion.button>
-							</motion.div>
-						<motion.img variants={textVariants} animate="scrollButton" src="/scroll.png" alt="scroll" />
-						</motion.div>
-					</div>
-					<motion.div
-					 className="slidingTextContainer"
-						 variants={sliderVariants}
-							initial="initial"
-							animate="animate"
-							>
-						CREATOR OF DIGITAL PIECES AND WEB CONTENT
+	return (
+		<div className="hero">
+			<div className="wrapper">
+				<motion.div
+					className="textContainer"
+					variants={textVariants}
+					initial="initial"
+					animate="animate"
+				>
+					<motion.h2 variants={textVariants}>PAULA QUINTERO</motion.h2>
+					<motion.h1 variants={textVariants}>Web designer</motion.h1>
+					<motion.div variants={textVariants} className="buttons">
+						<motion.button variants={textVariants}>See the latest Works</motion.button>
+						<motion.button variants={textVariants}>Contact Me</motion.button>
 					</motion.div>
-					<div className="imageContainer">
-						<img	src="/Hero.png" alt="Imagen Home" />
-					</div>
-					</div>
-		)
+					<motion.img variants={textVariants} animate="scrollButton" src="/scroll.png" alt="scroll" />
+				</motion.div>
+			</div>
+			<motion.div
+				className="slidingTextContainer"
+				variants={sliderVariants}
+				initial="initial"
+				animate="animate"
+			>
+				UX | UI | WEB | DESIGN 
+			</motion.div>
+			<div className="imageContainer">
+				<img src="/Hero.png" alt="Imagen Home" />
+			</div>
+		</div>
+	)
 }
 
 export default Hero
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,80 +1,95 @@
 import "./services.scss";
-import {motion} from "framer-motion";
+import	{ useRef } from "react";
+import { motion, useInView, } from "framer-motion";
 
 const variants = {
-	initial:{
-		x:-500,
-		y:100,
-		opacuty:0
+	initial: {
+		x: -500,
+		y: 100,
+		opacity: 0
 	},
-	animate:{
-		x:0,
+	animate: {
+		x: 0,
 		opacity: 1,
-		y:0,
-		transition:{
-			duration:1,
-			staggerChikdren:0.1,
-	 },
- },
+		y: 0,
+		transition: {
+			duration: 1,
+			staggerChikdren: 0.1,
+		},
+	},
 };
 
 
 const Services = () => {
-		return (
-				<motion.div className="services" variants={variants} initial="initial" animate="animate" >
-					<motion.div className="textContainer" variants={variants}>
-						<p>
-							I focus on helping you brand grow
-							<br/> and move forward
-						</p>
-						<hr />
-					</motion.div>
-					<motion.div className="titleContainer" variants={variants}>
-						<div className="title">
-							<img src="/work.png" alt="Wallpaper set up" />
-							<h1>
-								<b>Unique</b> Ideas
-							</h1>
-							</div>
-							<div className="title">
-							<h1>
-								<b>For You</b> Business.
-							</h1>
-							<button>WHAT WE DO?</button>
-						</div>
-						</motion.div>
-					<motion.div className="listContainer" variants={variants}>
-						<motion.div className="box" whileHover={{backgroundColor:"#e81ea483", color:"white", borderRadius:"50px"}}>
-							<h2>Branding</h2>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								 Et reprehenderit commodi totam iure molestias quos illum dolorum nobis dolores dolor consequatur cum rem,
-								 voluptas recusandae mollitia quo voluptate facilis soluta.</p>
-									<button>Go</button>
-						</motion.div>
-						<motion.div className="box"  whileHover={{backgroundColor:"#e81ea483",  color:"white",  borderRadius:"50px"}}>
-							<h2>Branding</h2>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								 Et reprehenderit commodi totam iure molestias quos illum dolorum nobis dolores dolor consequatur cum rem,
-								 voluptas recusandae mollitia quo voluptate facilis soluta.</p>
-									<button>Go</button>
-						</motion.div>
-						<motion.div className="box"  whileHover={{backgroundColor:"#e81ea483", color:"white",  borderRadius:"50px"}}>
-							<h2>Branding</h2>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								 Et reprehenderit commodi totam iure molestias quos illum dolorum nobis dolores dolor consequatur cum rem,
-								 voluptas recusandae mollitia quo voluptate facilis soluta.</p>
-									<button>Go</button>
-						</motion.div>
-						<motion.div className="box"  whileHover={{backgroundColor:"#e81ea483", color:"white",  borderRadius:"50px"}}>
-							<h2>Branding</h2>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								 Et reprehenderit commodi totam iure molestias quos illum dolorum nobis dolores dolor consequatur cum rem,
-								 voluptas recusandae mollitia quo voluptate facilis soluta.</p>
-									<button>Go</button>
-						</motion.div>
-					</motion.div>
-					</motion.div>
-		)
+
+	const ref = useRef();
+
+	const isInView = useInView(ref, { margin: "-100px" });
+
+	return (
+		<motion.div
+			className="services"
+			variants={variants}
+			initial="initial"
+			//animate="animate"  
+			// whileInView="animate"
+			ref={ref}
+			animate={isInView && "animate"}
+
+		>
+			<motion.div className="textContainer" variants={variants}>
+				<p>
+					I focus on helping you brand grow
+					<br /> and move forward
+				</p>
+				<hr />
+			</motion.div>
+			<motion.div className="titleContainer" variants={variants}>
+				<div className="title">
+					<img src="/work.png" alt="Wallpaper set up" />
+					<h1>
+						<motion.b whileHover={{color:"#0197af"}}>Unique</motion.b> Ideas
+					</h1>
+				</div>
+				<div className="title">
+					<h1>
+						<motion.b whileHover={{color:"#0197af"}}>For You</motion.b> Business.
+					</h1>
+					<button>WHAT WE DO?</button>
+				</div>
+			</motion.div>
+			<motion.div className="listContainer" variants={variants}>
+				<motion.div className="box" whileHover={{ backgroundColor: "#e81ea483", color: "white", borderRadius: "50px" }}>
+					<h2>Branding</h2>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Et reprehenderit commodi totam iure molestias quos illum dolorum nobis dolores dolor consequatur cum rem,
+						voluptas recusandae mollitia quo voluptate facilis soluta.</p>
+					<button>Go</button>
+				</motion.div>
+				<motion.div className="box" whileHover={{ backgroundColor: "#e81ea483", color: "white", borderRadius: "50px" }}>
+					<h2>Branding</h2>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Et reprehenderit commodi totam iure molestias quos illum dolorum nobis dolores dolor consequatur cum rem,
+						voluptas recusandae mollitia quo voluptate facilis soluta.</p>
+					<button>Go</button>
+				</motion.div>
+				<motion.div className="box" whileHover={{ backgroundColor: "#e81ea483", color: "white", borderRadius: "50px" }}>
+					<h2>Branding</h2>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Et reprehenderit commodi totam iure molestias quos illum dolorum nobis dolores dolor consequatur cum rem,
+						voluptas recusandae mollitia quo voluptate facilis soluta.</p>
+					<button>Go</button>
+				</motion.div>
+				<motion.div className="box" whileHover={{ backgroundColor: "#e81ea483", color: "white", borderRadius: "50px" }}>
+					<h2>Branding</h2>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Et reprehenderit commodi totam iure molestias quos illum dolorum nobis dolores dolor consequatur cum rem,
+						voluptas recusandae mollitia quo voluptate facilis soluta.</p>
+					<button>Go</button>
+				</motion.div>
+			</motion.div>
+		</motion.div>
+	)
 }
 
 export default Services
