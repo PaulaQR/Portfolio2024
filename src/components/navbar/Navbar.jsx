3986a162c5da4e6f11a8207a2b112314c4f20e3b
María Paula Quintero Rodriguez <79113076+PaulaQR@users.noreply.github.com>
2024-04-01 15:41:02 -0500

    Agregando links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,10 +13,10 @@ const Navbar = () => {
 					animate={{ opacity: 1, scale: 1 }}
 					transition={{ duration: 0.5 }}>PAUPIXEL</motion.span>
 				<div className="social">
-					<a href="#"><img src="/facebook.png" alt="logo facebook" /> </a>
-					<a href="#"><img src="/instagram.png" alt="logo instagram" /> </a>
-					<a href="#"><img src="/linkedin.png" alt="logo linkedin" /> </a>
-					<a href="#"><img src="/behance.png" alt="logo behance" /> </a>
+					<a href="https://www.facebook.com/profile.php?id=100049483710983&sk=photos"><img src="/facebook.png" alt="logo facebook" /> </a>
+					<a href="https://www.instagram.com/pau.pixel/?hl=es"><img src="/instagram.png" alt="logo instagram" /> </a>
+					<a href="https://www.linkedin.com/in/paulaquinteror/"><img src="/linkedin.png" alt="logo linkedin" /> </a>
+					<a href="https://www.behance.net/marapquinter1"><img src="/behance.png" alt="logo behance" /> </a>
 				</div>
 			</div>
 		</div>
